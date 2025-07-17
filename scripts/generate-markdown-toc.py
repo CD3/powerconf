c4ae554990b5ed9e3413f2ff0fbdc121c52c8a5e
CD Clark III <clifton.clark@gmail.com>
2025-07-16 20:44:52 -0500

    doc: still working on README TOC

---
+++ scripts/generate-markdown-toc.py
@@ -0,0 +1,29 @@
+import pathlib
+import re
+import sys
+
+
+def title_to_anchor(title: str):
+    return title.lower().replace(" ", "-")
+
+
+def main():
+    markdown_file = sys.argv[1]
+    markdown_file = pathlib.Path(markdown_file)
+
+    patterns = ["^\s*(#+)( .*)$"]
+    with markdown_file.open() as f:
+        for line in f:
+            line = line.strip()
+            for pattern in patterns:
+                m = re.match(pattern, line)
+                if m:
+                    level = m.group(1).strip()
+                    title = m.group(2).strip()
+                    anchor = title_to_anchor(title)
+                    prefix = level.replace("#", " ")
+                    print(f"{prefix}- [{title}]({anchor})")
+
+
+if __name__ == "__main__":
+    main()
