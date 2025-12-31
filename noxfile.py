1e23e6f7f569ef24230865112e11434975bd8f9b
C.D. Clark III <clifton.clark@gmail.com>
2025-12-31 07:29:18 -0600

    tests: got nox working

--- noxfile.py
+++ noxfile.py
@@ -2,23 +2,23 @@
 
 import nox
 
-package = "fspathtree/"
-python_versions = ["3.7", "3.8", "3.9", "3.10"]
+python_versions = ["3.12"]
 nox.needs_version = ">= 2021.6.6"
-nox.options.sessions = ("tests",)
+nox.options.sessions = ("tests", "lint")
 
 
 @nox.session(python=python_versions)
 def tests(session: nox.Session) -> None:
     """Run the test suite."""
-    session.install(".")
-    session.install("poetry")
-    session.run("poetry", "install", "--with", "dev")
+    session.install("uv")
+    session.run("uv", "pip", "install", "-e", ".", "--group", "dev")
     session.run("pytest", *session.posargs)
 
 
 @nox.session
 def lint(session: nox.Session) -> None:
-    """Run th elinter"""
-    session.install("pre-commit")
-    session.run("pre-commit", "run", "--all-file", *session.posargs)
+    """Run the linter."""
+    session.install("uv")
+    session.run("uv", "pip", "install", "-e", ".", "--group", "dev")
+    session.run("ruff", "check", ".", "--fix")
+    session.run("ruff", "check", ".")
