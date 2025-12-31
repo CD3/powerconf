d7171061875f7f0af8f2e2300128092066a2b99f
C.D. Clark III <clifton.clark@gmail.com>
2025-12-31 07:30:31 -0600

    style: fixed lint error

--- src/powerconf/schema.py
+++ src/powerconf/schema.py
@@ -1,22 +1,22 @@
-from typing import Annotated, List
+from typing import Annotated
 
-import numpy
-from pydantic import (AfterValidator, AliasChoices, BaseModel, Field,
+from pydantic import (AfterValidator, AliasChoices, Field,
                       PlainSerializer, WithJsonSchema)
 
 from .units import make_quantity
 
-QuantityWithUnit = lambda U, names=None, desc=None: Annotated[
-    str,
-    AfterValidator(lambda x: make_quantity(x).to(U)),
-    PlainSerializer(lambda x: f"{x:~}" if x is not None else "null", return_type=str),
-    WithJsonSchema({"type": "string"}, mode="serialization"),
-    Field(
-        alias=(
-            AliasChoices(*names if type(names) is list else names)
-            if names is not None
-            else None
+def QuantityWithUnit(U, names=None, desc=None):
+    return Annotated[
+        str,
+        AfterValidator(lambda x: make_quantity(x).to(U)),
+        PlainSerializer(lambda x: f"{x:~}" if x is not None else "null", return_type=str),
+        WithJsonSchema({"type": "string"}, mode="serialization"),
+        Field(
+            alias=(
+                AliasChoices(*names if isinstance(names, list) else [names])
+                if names is not None
+                else None
+            ),
+            description=desc,
         ),
-        description=desc,
-    ),
-]
+    ]
